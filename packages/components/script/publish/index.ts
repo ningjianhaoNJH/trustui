6ef7be52007ee61fe5c81a0b90727fb66426e982
ningjianhaoNJH <375332835@qq.com>
2023-04-20 16:43:10 +0800

    update:  publish name

--- packages/components/script/publish/index.ts
+++ packages/components/script/publish/index.ts
@@ -1,7 +1,17 @@
+/*
+ * @Author: ningjianhaoNJH 375332835@qq.com
+ * @Date: 2023-04-20 16:07:03
+ * @LastEditors: ningjianhaoNJH 375332835@qq.com
+ * @LastEditTime: 2023-04-20 16:42:23
+ * @FilePath: \trustui\packages\components\script\publish\index.ts
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
 import run from '../utils/run';
 import { pkgPath } from '../utils/paths';
 import { series } from 'gulp';
 export const publishComponent = async () => {
-  run('release-it', `${pkgPath}/trustui`);
+  run('release-it', `${pkgPath}/njh-trustui`);
 };
 export default series(async () => publishComponent());
